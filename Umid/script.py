0d45f0098ce657a8aa22f94c0921169d12320970
Umid Ali <aliumid07@gmail.com>
2020-07-25 17:17:54 +0000

    party

---
+++ Umid/script.py
@@ -0,0 +1,37 @@
+# num= input("Please enter a number: ")
+# num = int(num)
+
+# if num % 2 == 0:
+#     print("Your number is even!")
+# else:
+#     print:("Your number is odd.")
+
+# elif = else if
+# four = 2 + 2
+# ten = 5 + 5
+# three = 2 + 3
+
+# if four != ten and three != four and bool(three) != False:
+#     print("These are all unequal")
+# elif bool(three) == True:
+#     print(three + " is True!")
+# else:
+#     print("Any other condition")
+
+name = input("Who are you?: ")
+
+age = input("How old are you?: ")
+age = int(age)
+
+if age > 18:
+    print("Welcome to the party!")
+else:
+    print("Looks like we got a young gun on our hands.")
+
+pizza = input("Did you bring pizza?: ")
+# pizza = int(pizza)
+
+if pizza == 'yes':
+    print("We're delighted to have you!")
+else:
+    print("I'm sure we'll find a use for you eventually.")

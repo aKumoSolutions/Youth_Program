bb8dc8e5b548137e38f0e9bc4101d660b77ca635
“SharifAbdulcoder” <abdugofir@gmail.com>
2020-07-25 12:31:36 -0500

    Umid's script

--- Umid/script.py
+++ Umid/script.py
@@ -23,15 +23,28 @@ name = input("Who are you?: ")
 age = input("How old are you?: ")
 age = int(age)
 
-if age > 18:
+
+if age > 18:    
     print("Welcome to the party!")
 else:
     print("Looks like we got a young gun on our hands.")
 
 pizza = input("Did you bring pizza?: ")
 # pizza = int(pizza)
+maybeAnswer = ['maybe', 'Maybe', 'mybe', 'maybe', 'okay']
+yesAnswer = ['yesh', 'ye', 'y', 'Y', 'yeah', 'yes', 'Yes']
+noAnswer = ['NO', 'n', 'no', 'N', 'n0', 'nah', 'nope', 'not']
 
-if pizza == 'yes':
+if pizza == yesAnswer:
     print("We're delighted to have you!")
-else:
+
+
+elif pizza == noAnswer:
     print("I'm sure we'll find a use for you eventually.")
+
+
+elif pizza == maybeAnswer:
+    print("You seem unreliable...who are you really?")
+
+else: 
+    print("We're taking you to into the basement")
